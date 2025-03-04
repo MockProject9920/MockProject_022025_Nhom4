6c7e795cb4d8c3047e22e51362c5b8689a002038
hoang viet an <hoangan22092002@gmail.com>
2025-03-04 14:41:44 +0700

    initial commit

---
+++ MockProject_022025_Nhom4/MockProject_022025_Nhom4/src/main/java/com/javaweb/MockProject_022025_Nhom4/MockProject022025Nhom4Application.java
@@ -0,0 +1,13 @@
+package com.javaweb.MockProject_022025_Nhom4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockProject022025Nhom4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockProject022025Nhom4Application.class, args);
+	}
+
+}
---
+++ MockProject_022025_Nhom4/MockProject_022025_Nhom4/src/test/java/com/javaweb/MockProject_022025_Nhom4/MockProject022025Nhom4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaweb.MockProject_022025_Nhom4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockProject022025Nhom4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
