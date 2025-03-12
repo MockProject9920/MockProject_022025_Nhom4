8cc54bf6384a37292492a48805f8ef2e16e5e9eb
trongnb02 <tronglab1337@gmail.com>
2025-03-12 14:18:39 +0700

    Basic CRUD for Property

--- mockproject/src/main/java/com/group4/mockproject/service/PropertyService.java
+++ mockproject/src/main/java/com/group4/mockproject/service/PropertyService.java
@@ -1,5 +1,8 @@
 package com.group4.mockproject.service;
 
+import com.group4.mockproject.dto.request.CreateProperty;
+import com.group4.mockproject.exception.ResourceNotFoundException;
+import com.group4.mockproject.mapper.PropertyMapper;
 import com.group4.mockproject.model.Property;
 import com.group4.mockproject.repository.PropertyRepository;
 import lombok.AllArgsConstructor;
@@ -14,8 +17,30 @@ public class PropertyService {
     @Autowired
     private final PropertyRepository propertyRepository;
 
+    @Autowired
+    private final PropertyMapper propertyMapper;
+
     public List<Property> getAllProperties(){
         return propertyRepository.findAll();
     }
 
+    public Property createProperty(CreateProperty request){
+        return propertyRepository.save(propertyMapper.toEntity(request));
+    }
+
+    public Property getPropertyById(Long propertyId){
+        return propertyRepository.findById(propertyId).orElseThrow(() -> new ResourceNotFoundException("Property not found!"));
+    }
+
+    public Property updateProperty(Long propertyId, CreateProperty request){
+        Property existingProperty = getPropertyById(propertyId);
+        Property updatedProperty = propertyMapper.toEntity(request);
+        updatedProperty.setId(existingProperty.getId());
+        return propertyRepository.save(updatedProperty);
+    }
+
+    public void deletePropertyById(Long propertyId){
+        Property existingProperty = getPropertyById(propertyId);
+        propertyRepository.delete(existingProperty);
+    }
 }
